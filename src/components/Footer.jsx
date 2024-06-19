d2856f2c9aea71e5e979c901ea14b6562b839025
Callum Inglis <callum.inglis@4oh4.co.uk>
2024-06-19 17:23:46 +0100

    a footer of sorts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 const Footer = () => {
     return (
         <footer>
-            <p>By <a href='https://calluminglis.com?src=react-kanban-board'>Callum Inglis</a></p>
+            <div className="tips">
+                <b>Tips</b>
+                <ul>
+                    <li>Double click a task to toggle the blocked status. </li>
+                    <li>Drag and drop tasks between columns to change their status.</li>
+                </ul>
+            </div>
+            <div className="about">
+                <b>About</b>
+                <p>By <a href='https://calluminglis.com?src=react-kanban-board'>Callum Inglis</a></p>
+            </div>
         </footer>
     )
 };
